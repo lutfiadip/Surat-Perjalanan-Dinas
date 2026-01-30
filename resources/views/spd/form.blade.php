11a3656005b1e3f9a4c7f594135733d71c7a3f8a
Lutfi Adi P <lutfiadiprakoso@students.unnes.ac.id>
2026-01-30 11:06:31 +0700

    mengedit tampilan login dan form

--- resources/views/spd/form.blade.php
+++ resources/views/spd/form.blade.php
@@ -330,7 +330,7 @@
                     <div class="grid" style="grid-template-columns: 1fr 1fr 1fr;">
                         <div class="form-group">
                             <label>Nomor Surat</label>
-                            <input type="text" name="nomor_surat" placeholder="contoh: 094 / 123 / XII / 2025" value="{{ old('nomor_surat', $draft->nomor_surat ?? '') }}">
+                            <input type="text" name="nomor_surat" placeholder="contoh:        /        / XII / 2025" value="{{ old('nomor_surat', $draft->nomor_surat ?? '') }}">
                         </div>
                         <div class="form-group">
                             <label>Tanggal Surat</label>
