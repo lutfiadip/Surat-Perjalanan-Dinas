c6ec6152bcbf6b43ad033a0bae13d5ca9523a36f
Lutfi Adi P <lutfiadiprakoso@students.unnes.ac.id>
2026-02-18 15:09:02 +0700

    mengedit tampilan

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -359,24 +359,7 @@
                                                 </button>
                                             </form>
 
-                                            <form action="{{ route('admin.users.destroy', $item->id) }}" method="POST"
-                                                class="inline-block"
-                                                onsubmit="return confirm('Apakah Anda yakin ingin menghapus user ini? Tindakan ini tidak dapat dibatalkan.')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="p-2 text-red-400 hover:text-red-600 hover:bg-red-50 rounded-lg transition-all"
-                                                    title="Hapus Permanen">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24"
-                                                        fill="none" stroke="currentColor" stroke-width="2"
-                                                        stroke-linecap="round" stroke-linejoin="round">
-                                                        <polyline points="3 6 5 6 21 6"></polyline>
-                                                        <path
-                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                                                        </path>
-                                                    </svg>
-                                                </button>
-                                            </form>
+
                                         @endif
                                     </td>
                                 </tr>
